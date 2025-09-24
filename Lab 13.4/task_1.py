4e0e0f9485616c96f6b8b293048a66c3c36d8e9d
Maneesha2006 <2403a51040@sru.edu.in>
2025-09-24 14:47:34 +0530

    Delete Lab 13.4 directory

--- Lab 13.4/task_1.py
+++
@@ -1,6 +0,0 @@
-numbers_input = input("Enter numbers separated by spaces: ")
-numbers = [int(n) for n in numbers_input.split()]
-squares = [n ** 2 for n in numbers]
-
-# Print the result
-print(squares)
